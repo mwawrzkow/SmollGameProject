489cd0675a2bb8a3340217c9ec1a4efddf75a0f4
Andrzej Szafranski <szafranski.andrzej1999@gmail.com>
2022-07-03 00:09:13 +0200

    added a small white square in the right bottom corner

--- main.cpp
+++ main.cpp
@@ -4,15 +4,18 @@
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(sf::Vector2u(1920,1800)),"Works"); 
+  sf::RenderWindow window(sf::VideoMode(sf::Vector2u(1920,1080)),"Works"); 
   sf::CircleShape shape(100.f);
   sf::CircleShape shape1(50.0f); 
   sf::CircleShape shape2(300.f);
+  sf::RectangleShape shape3(sf::Vector2f(5,5));
   shape1.setPosition(sf::Vector2f(200,500));
   shape1.setFillColor(sf::Color::Green);
   shape.setFillColor(sf::Color::Red);
   shape2.setPosition(sf::Vector2f(500,200));
   shape2.setFillColor(sf::Color::Blue);
+  shape3.setPosition(sf::Vector2f(1915,1075));
+  shape3.setFillColor(sf::Color::White);
   while(window.isOpen()){ 
   sf::Event event; 
   while(window.pollEvent(event)){ 
@@ -25,6 +28,7 @@ int main()
   window.draw(shape); 
   window.draw(shape1);
   window.draw(shape2);
+  window.draw(shape3);
   window.display();
   } 
 }
