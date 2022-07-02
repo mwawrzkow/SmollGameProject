e53169cc0f7750ece36e72d7e7126a6e2c760995
Andrzej Szafranski <szafranski.andrzej1999@gmail.com>
2022-07-02 23:03:02 +0200

    added a large blue circle

--- main.cpp
+++ main.cpp
@@ -7,9 +7,12 @@ int main()
   sf::RenderWindow window(sf::VideoMode(sf::Vector2u(1920,1800)),"Works"); 
   sf::CircleShape shape(100.f);
   sf::CircleShape shape1(50.0f); 
+  sf::CircleShape shape2(300.f);
   shape1.setPosition(sf::Vector2f(200,500));
   shape1.setFillColor(sf::Color::Green);
   shape.setFillColor(sf::Color::Red);
+  shape2.setPosition(sf::Vector2f(500,200));
+  shape2.setFillColor(sf::Color::Blue);
   while(window.isOpen()){ 
   sf::Event event; 
   while(window.pollEvent(event)){ 
@@ -21,6 +24,7 @@ int main()
   window.clear();
   window.draw(shape); 
   window.draw(shape1);
+  window.draw(shape2);
   window.display();
   } 
 }
